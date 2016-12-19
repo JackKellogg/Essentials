b878d1c64904f72e139a149f8b7665923f328540
Jack Kellogg <Jack@Jacks-MacBook-Pro-2.local>
2016-12-19 01:04:37 -0600

    Added commands package Gamemode command Command Executor in main class

--- src/main/java/com/skiheal/essentials/Essentials.java
+++ src/main/java/com/skiheal/essentials/Essentials.java
@@ -1,4 +1,15 @@
 package com.skiheal.essentials;
 
-public class Essentials {
+import com.skiheal.essentials.commands.Gamemode;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Essentials extends JavaPlugin {
+
+    public void onEnable() {
+        registerCommands();
+    }
+
+    public void registerCommands(){
+        getCommand("gm").getExecutor(new Gamemode());
+    }
 }
